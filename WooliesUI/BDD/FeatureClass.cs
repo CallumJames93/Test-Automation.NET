432d6ea52c3c2c98795beb72bc60c4602bb1e4c4
Callum <callum.gill1993@hotmail.co.uk>
2020-03-13 08:34:15 +1100

    Third commit to add example  Specflow BDD

---
+++ WooliesUI/BDD/FeatureClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace WooliesUI.BDD
+{
+    public class FeatureClass
+    {
+        public FeatureClass()
+        {
+            /* Could not add  feature file on mac for some reaosn. But here is example code for Specflow
+             *
+             * Feature: Weather
+             In order to not get wet
+             As a weather enthusiast 
+             I want to be told the weather in Sydney
+
+             @mytag
+            Scenario: A happy holiday maker
+            Given I like to holiday in Sydney
+            And I only like to holiday  on Thursdays
+            When I lookup the weather forecast
+            Then I receive the weather forecast
+            And the temperature is hotter than 10 degrees
+
+             */
+        }
+    }
+}
